c3990cf2b24ddc6e275013d323b57100dec6326e
Cathy Y F <cathyyfu@gmail.com>
2024-02-13 17:19:11 -0800

    Updated SwapEnds to support punctuation and added test file

--- src/main/java/logic/codes/SwapEnds.java
+++ src/main/java/logic/codes/SwapEnds.java
@@ -1,6 +1,8 @@
 package logic.codes;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SwapEnds extends Code {
     
@@ -10,33 +12,59 @@ public class SwapEnds extends Code {
     }
 
     private String swap(String input) {
-        String[] words = input.split(" ");
-
-        StringBuilder finalWords = new StringBuilder();
-        if (input.charAt(0) == ' ') {
-            finalWords.append(" ");
+        if (input.trim().isEmpty()) {
+            return input;
         }
 
-        for(String word : words) {
-            if (word.length() == 1) {
-                finalWords.append(word + " ");
-                continue;
-            }
+        Matcher matchedWordBoundaries = Pattern.compile("[^a-zA-Z0-9'-]").matcher(input);
+
+        StringBuilder finalWords = new StringBuilder();
+
+        int wordStartIndex = 0;
+        int wordEndIndex = 0;
+        while(matchedWordBoundaries.find()) {
+            wordEndIndex = matchedWordBoundaries.start();
+
+            swapEndsImpl(input, wordStartIndex, wordEndIndex, finalWords);
+
+            wordStartIndex = matchedWordBoundaries.end();
+        }
+
+        if (wordStartIndex < input.length() - 1) {
+            swapEndsImpl(input, wordStartIndex, input.length(), finalWords);
+        }
+
+        return finalWords.toString();
+    }
+
+    private void swapEndsImpl(String input, int wordStartIndex, int wordEndIndex, StringBuilder finalWords) {
+        String word = input.substring(wordStartIndex, wordEndIndex);
+
+        if (word.isEmpty()) {
+            // preserve symbols
+            finalWords.append(input.charAt(wordStartIndex));
+        } else if (word.length() == 1) {
+            finalWords.append(word);
+            appendMatcher(input, wordEndIndex, finalWords);
+        } else {
             char[] letters =  word.toCharArray();
 
             StringBuilder finalWord = new StringBuilder();
             finalWord.append(letters[letters.length-1]);
             finalWord.append(Arrays.copyOfRange(letters, 1, letters.length-1));
             finalWord.append(letters[0]);
+            finalWords.append(finalWord);
 
-            finalWords.append(finalWord + " ");
+            appendMatcher(input, wordEndIndex, finalWords);
+        }
+    }
+
+    public void appendMatcher(String input, int index, StringBuilder finalWords) {
+        if ((input.length() - 1) < index) {
+            return;
         }
 
-        if (input.charAt(input.length()-1) != ' ') {
-            finalWords.deleteCharAt(finalWords.length()-1);
-        }
-
-        return finalWords.toString();
+        finalWords.append(input.charAt(index));
     }
 
     @Override
---
+++ src/test/java/codes/SwapEndsTest.java
@@ -0,0 +1,60 @@
+package codes;
+
+import logic.codes.PigLatin;
+import logic.codes.SwapEnds;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SwapEndsTest {
+
+    // since stateless, we declare one instance for test
+    SwapEnds swapEnds = new SwapEnds();
+
+    private static class TestParams {
+        String input;
+        String expectedOutput;
+
+        TestParams(String input, String expectedOutput){
+            this.input = input;
+            this.expectedOutput = expectedOutput;
+        }
+    }
+
+    @Test
+    public void givenEmptyString_testMorph_expectEmptyString() {
+        SwapEndsTest.TestParams zeroLengthString = new SwapEndsTest.TestParams("", "");
+        SwapEndsTest.TestParams stringWithNoWords = new SwapEndsTest.TestParams("    ","    ");
+
+        Assertions.assertEquals(zeroLengthString.expectedOutput , swapEnds.morph(zeroLengthString.input));
+        Assertions.assertEquals(stringWithNoWords.expectedOutput , swapEnds.morph(stringWithNoWords.input));
+    }
+
+    @Test
+    public void givenPhraseDifferentLeadingOrTrailingSpaces_testMorph_expectSwapEndsPhrase() {
+        /*
+            This phrase does not have leading or trailing spaces
+         */
+        SwapEndsTest.TestParams phraseOne = new SwapEndsTest.TestParams("no spaces","on spaces");
+
+        /*
+            This phrase does not have spaces at all
+         */
+        SwapEndsTest.TestParams phraseTwo = new SwapEndsTest.TestParams("whatarespaceseven","nhatarespacesevew");
+
+        /*
+            This has multiple leading and trailing spaces
+         */
+        SwapEndsTest.TestParams phraseThree = new SwapEndsTest.TestParams("   WeeeOneBigWordy     ","   yeeeOneBigWordW     ");
+
+        Assertions.assertEquals(phraseOne.expectedOutput , swapEnds.morph(phraseOne.input));
+        Assertions.assertEquals(phraseTwo.expectedOutput , swapEnds.morph(phraseTwo.input));
+        Assertions.assertEquals(phraseThree.expectedOutput , swapEnds.morph(phraseThree.input));
+    }
+
+    @Test
+    public void givenPhraseWithSymbols_testMorph_expectSwapEndsPhrase() {
+        SwapEndsTest.TestParams punctuationString = new SwapEndsTest.TestParams(" Should've could've would've but Nope?!?.", " ehould'vS eould'vc eould'vw tub eopN?!?.");
+
+        Assertions.assertEquals(punctuationString.expectedOutput , swapEnds.morph(punctuationString.input));
+    }
+}
